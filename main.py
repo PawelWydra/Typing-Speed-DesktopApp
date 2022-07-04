b1dc46ce35398859020be2cb8148e5fc435e011c
Pawel <wydrapawel@icloud.com>
2022-07-04 19:10:29 +0200

    string count

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 with open("sample_string.txt") as file:
     data = file.readline()
 
-print(data.split(" "))
+count_list = data.split(" ")
+count_start = (len(count_list))
+given_text = input("gimme text")
+
+entered_list = given_text.split(" ")
+
+for word in entered_list:
+    if word in count_list:
+        count_list.pop(count_list.index(word))
+
+count_stop = count_start - (len(count_list))
