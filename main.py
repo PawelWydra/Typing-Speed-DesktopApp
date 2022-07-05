d85b3b3cffe0a345e3b8847e3148f1c2622ed207
Pawel <wydrapawel@icloud.com>
2022-07-05 08:04:13 +0200

    refactor code - logic working

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 def open_file():
     with open("sample_string.txt") as file:
         data = file.readline()
-        string_list = list(data)
-        return string_list
+        return list(data)
+
+
+def check_char(input_char, check_character):
+    global WORD_COUNT, WORD
+    ### check backspace ###
+    if input_char == "\b":
+        WORD = -1
+        return False
+    elif check_character == input_char:
+        if input_char == " ":
+            WORD_COUNT += 1
+    elif check_character != input_char:
+        print("wrong")
 
 
 WORD_COUNT = 1
-
+WORD = 0
+string_list = open_file()
 given_text = input("gimme text")
 entered_list = list(given_text)
-
-word = 0
-for _ in range(len(entered_list)):
-    if string_list[word] == entered_list[word]:
-        if entered_list[word] == " ":
-            WORD_COUNT += 1
-    elif string_list[word] != entered_list[word]:
-        print("wrong")
+for char in range(len(entered_list)):
+    if check_char(entered_list[char], string_list[char]):
+        WORD += 1
     else:
-        print("check")
-
-    word += 1
+        ### check word_count, without this space and backspace will multiply word_count var. ###
+        if char == " ":
+            WORD_COUNT -= 1
 print(WORD_COUNT)
